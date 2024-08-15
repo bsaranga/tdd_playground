38958e7d1a27e1a0fd064e1c62104268252482e0
Buwaneka Saranga <bsaranga@gmail.com>
2024-08-15 12:21:47 +0530

    feat: Add Cypress for end-to-end testing
    This commit adds Cypress to the project for end-to-end testing. It includes the necessary configuration files and fixtures for writing and running tests. Cypress provides a user-friendly interface for writing and executing tests, making it easier to ensure the functionality and reliability of the todo app.

---
+++ todo_app/cypress/e2e/spec.cy.js
@@ -0,0 +1,56 @@
+describe('Visits todo application on localhost', () => {
+  it('passes', () => {
+    cy.visit('http://localhost:3000')
+  })
+})
+
+describe('Task prerequisites', () => {
+  it('Task form is present', () => {
+    cy.visit('http://localhost:3000')
+    cy.get('#taskInputForm').should('exist')
+
+    cy.get('#taskInputForm label[for="title"]').should('exist').and('have.text', 'Title')
+    cy.get('#taskInputForm input[name="title"]').should('exist')
+
+    cy.get('#taskInputForm label[for="description"]').should('exist').and('have.text', 'Description')
+    cy.get('#taskInputForm input[name="description"]').should('exist')
+
+    cy.get('#taskInputForm label[for="dueDate"]').should('exist').and('have.text', 'Due Date')
+    cy.get('#taskInputForm input[name="dueDate"]').should('exist')
+
+    cy.get('#taskInputForm label[for="time"]').should('exist').and('have.text', 'Time')
+    cy.get('#taskInputForm input[name="time"]').should('exist')
+
+    cy.get('#taskInputForm label[for="priority"]').should('exist').and('have.text', 'Priority')
+    cy.get('#taskInputForm select[name="priority"]').should('exist')
+
+    cy.get('#taskInputForm button[type="submit"]').should('exist').and('have.text', 'Add Task')
+  });
+
+  it('Task list is present', () => {
+    cy.visit('http://localhost:3000')
+    cy.get('#taskList').should('exist')
+    cy.get('#taskList').children().should('have.length', 0);
+  });
+});
+
+describe('Task creation', () => {
+  it('Task is created', () => {
+    cy.visit('http://localhost:3000')
+    cy.get('#taskInputForm input[name="title"]').type('Task 1')
+    cy.get('#taskInputForm input[name="description"]').type('Task 1 description')
+    cy.get('#taskInputForm input[name="dueDate"]').type('2021-12-31')
+    cy.get('#taskInputForm input[name="time"]').type('12:00')
+    cy.get('#taskInputForm select[name="priority"]').select('High')
+
+    cy.get('#taskInputForm button[type="submit"]').click()
+
+    cy.get('#taskList').children().should('have.length', 1);
+    cy.get('#taskList').children().first().children().should('have.length', 5);
+    cy.get('#taskList').children().first().children().eq(0).should('have.text', 'Task 1');
+    cy.get('#taskList').children().first().children().eq(1).should('have.text', 'Task 1 description');
+    cy.get('#taskList').children().first().children().eq(2).should('have.text', '2021-12-31');
+    cy.get('#taskList').children().first().children().eq(3).should('have.text', '12:00');
+    cy.get('#taskList').children().first().children().eq(4).should('have.text', 'High');
+  });
+});
--- todo_app/routes/index.js
+++ todo_app/routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { tasks: [] });
+});
+
+router.post('/', function(req, res, next) {
+  console.log(req.body);
+  res.render('index', { tasks: [req.body] });
 });
 
 module.exports = router;
