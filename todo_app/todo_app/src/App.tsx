ae169d7924d161cc83acc22f923ac218ae7e1eeb
Saranga B <bsaranga@gmail.com>
2024-08-18 23:49:22 +0530

    feat: Add task deletion functionality to the todo app

--- todo_app/todo_app/src/App.tsx
+++ todo_app/todo_app/src/App.tsx
@@ -1,20 +1,52 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import config from '../config'
 import './App.css'
 
 const { todoApi } = config
 
-function App() {
+interface Task {
+  id: number
+  title: string
+  description: string
+  done: boolean
+  dueDate: string
+  priority: 'High' | 'Medium' | 'Low'
+}
 
+function App() {
+  const [tasks, setTasks] = useState<Task[]>([])
+  
   useEffect(() => {
     fetch(`${todoApi}/tasks`)
       .then(response => response.json())
-      .then(data => console.log(data))
-  })
+      .then(data => setTasks(data))
+  }, [])
+
+  function handleDelete(id: number) {
+    fetch(`${todoApi}/tasks/${id}`, {
+      method: 'DELETE'
+    })
+      .then(response => response.json())
+      .then(data => {
+        if (data) {
+          setTasks(data)
+        }
+      })
+  }
   
   return (
     <>
-      <p>hello world</p>
+      {
+        tasks.map(t => {
+          return (
+            <div key={t.id}>
+              <h3>{t.title}</h3>
+              <p>{t.description}</p>
+              <button onClick={() => handleDelete(t.id)}>Delete</button>
+            </div>
+          )
+        })
+      }
     </>
   )
 }
