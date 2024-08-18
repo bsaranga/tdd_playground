6dc9b0693b26fa5239881c24059460c4103cf959
Saranga B <bsaranga@gmail.com>
2024-08-18 20:55:09 +0530

    playwright stuff

--- todo_app/tests/example.spec.ts
+++ todo_app/tests/example.spec.ts
@@ -11,8 +11,13 @@ test('get started link', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  const element = await page.getByRole('link', { name: 'Get started' });
+  
+  if (element) {
+    element.click();
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+    // Expects page to have a heading with the name of Installation.
+    await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  }
+  
 });
