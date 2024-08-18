d7074731f8f9ffe10970738e0f394117ad4999cd
Saranga B <bsaranga@gmail.com>
2024-08-18 23:36:43 +0530

    CORS settings and etc

--- todo_app/todo_api/app.js
+++ todo_app/todo_api/app.js
@@ -2,10 +2,16 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 
 var app = express();
+
+app.use(cors({ 
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'DELETE', 'PUT', 'PATCH'],
+}));
 
 app.use(logger('dev'));
 app.use(express.json());
