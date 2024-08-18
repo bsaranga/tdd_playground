dbb5039028df2e2af239376e5b1c6d5e5093922e
Saranga B <bsaranga@gmail.com>
2024-08-18 23:23:54 +0530

    todo api surface complete

--- todo_app/todo_api/routes/index.js
+++ todo_app/todo_api/routes/index.js
@@ -14,6 +14,32 @@ router.get('/tasks', function(req, res, next) {
   }
 });
 
+router.get('/tasks/search', function(req, res, next) {
+  try {
+    
+    const { title } = req.query;
+    console.log(title);
+
+    const tasks = todoManager.searchTasksByTitle(title);
+    res.status(200).json(tasks);
+
+  } catch (error) {
+    
+    res.status(500).json({ error: "An uncaught error occurred." });
+
+  }
+});
+
+router.get('/tasks/:id', function(req, res, next) {
+  try {
+    const { id } = req.params;
+    const task = todoManager.getTaskById(id);
+    res.status(200).json(task);
+  } catch (error) {
+    res.status(500).json({ error: "An uncaught error occurred." });
+  }
+});
+
 router.post('/tasks', function(req, res, next) {
   
   try {
@@ -34,6 +60,53 @@ router.post('/tasks', function(req, res, next) {
   }
 });
 
+router.put('/tasks/:id', function(req, res, next) {
+  try {
+    
+    const { id } = req.params;
+    const { title, description, dueDateStr, dueTimeStr, priority } = req.body;
+    const dueDate = createDate(dueDateStr, dueTimeStr);
+
+    todoManager.editTask(id, title, description, new Date(dueDate), priority);
+    res.status(200).json(todoManager.getTaskById(id));
+
+  } catch (error) {
+    
+    res.status(500).json({ error: "An uncaught error occurred." });
+  }
+});
+
+router.patch('/tasks/:id', function(req, res, next) {
+  try {
+    
+    const { id } = req.params;
+    const { done } = req.body;
+
+    if (done === undefined) {
+      const malformedReq = new Error("Malformed request");
+      malformedReq.name = "MalformedRequestError";
+      throw malformedReq;
+    }
+    
+    if (done === true) {
+      todoManager.markAsDone(id);
+    } else {
+      todoManager.markAsUndone(id);
+    }
+
+    res.status(200).json(todoManager.getTaskById(id));
+
+  } catch (error) {
+    
+    if (error.name === "MalformedRequestError") {
+      res.status(400).json({ error: error.message });
+    }
+
+    res.status(500).json({ error: "An uncaught error occurred." });
+
+  }
+});
+
 router.delete('/tasks/:id', function(req, res, next) {
   try {
     
@@ -46,6 +119,6 @@ router.delete('/tasks/:id', function(req, res, next) {
     res.status(500).json({ error: "An uncaught error occurred." });
 
   }
-})
+});
 
 module.exports = router;
